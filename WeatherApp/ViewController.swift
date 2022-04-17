680efa0f6e2808f3878bc742b2aaf791ee7cb6f3
stasyambaa <stas.shapetko93@gmail.com>
2022-04-17 15:01:22 +0200

    outlets

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTemperatureLabel: UILabel!
+    
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
