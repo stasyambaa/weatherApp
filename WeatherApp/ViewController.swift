e4a7b0a5e0c5f7d465f6c4413b5056274fdc21e5
stasyambaa <stas.shapetko93@gmail.com>
2022-04-17 14:18:27 +0200

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Станислав Шапетько on 17.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
